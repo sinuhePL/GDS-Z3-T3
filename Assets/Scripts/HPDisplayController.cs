a4005fd24fb9a43c73f252b2e52b52ca67269239
sinuhePL <lawicki.karol@gmail.com>
2021-08-27 00:25:20 +0200

    Added enemy HP display animation

--- Assets/Scripts/HPDisplayController.cs
+++ Assets/Scripts/HPDisplayController.cs
@@ -10,25 +10,34 @@ namespace GDS3
         public IntegerReference _currentHP;
         public GameObject _hitPointMarkerPrefab;
 
-        private ParticleSystem[] _hitPointsMarkers;
+        private List<ParticleSystem> _hitPointsMarkers;
 
         private void Start()
         {
-            _hitPointsMarkers = new ParticleSystem[_initialHP.Value];
+            _hitPointsMarkers = new List<ParticleSystem>();
             for(int i = 0; i < _initialHP.Value; i++)
             {
-                _hitPointsMarkers[i] = Instantiate(_hitPointMarkerPrefab, transform.position + new Vector3(i-1, 3.0f, 0.0f), Quaternion.identity, transform).GetComponent<ParticleSystem>();
+                _hitPointsMarkers.Add(Instantiate(_hitPointMarkerPrefab, transform.position + new Vector3(Mathf.Cos(2 * 3.1415f * (i+1)/_initialHP.Value), Mathf.Sin(2 * 3.1415f * (i+1) / _initialHP.Value) + 3.0f, 0.0f), Quaternion.identity, transform).GetComponent<ParticleSystem>());
             }
         }
 
         public void UpdateDisplay()
-        { 
-            for (int i = 0; i < _initialHP.Value; i++)
+        {
+            ParticleSystem systemToDestroy;
+
+            systemToDestroy = _hitPointsMarkers[_hitPointsMarkers.Count - 1];
+            _hitPointsMarkers.RemoveAt(_hitPointsMarkers.Count - 1);
+            Destroy(systemToDestroy);
+        }
+
+        private void Update()
+        {
+            float currentTime = Time.time;
+            int i = 0;
+            foreach(ParticleSystem hitPoint in _hitPointsMarkers)
             {
-                if(i == _currentHP.Value)
-                {
-                    Destroy(_hitPointsMarkers[i]);
-                }
+                hitPoint.transform.localPosition = new Vector3(Mathf.Cos(currentTime%(2 * 3.1415f) + 2 * 3.1415f * (i+1) / _initialHP.Value), Mathf.Sin(currentTime%(2 * 3.1415f) + 2 * 3.1415f * (i+1) / _initialHP.Value) * 0.3f + 4.0f, 0.0f) * 5.0f;
+                i++;
             }
         }
     }
