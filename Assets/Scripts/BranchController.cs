c6eb871e5e55bc5fe5009254ad530a8e37d8f042
sinuhePL <lawicki.karol@gmail.com>
2021-08-30 16:30:36 +0200

    Added moving branch for 3rd riddle

---
+++ Assets/Scripts/BranchController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GDS3
+{
+    public class BranchController : MonoBehaviour
+    {
+        public BoolReference _isPlayerSmall;
+        public Animator _myAnimator;
+        public BoxCollider2D _myCollider;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Player")
+            {
+                BendBranch();
+            }
+        }
+
+        public void BendBranch()
+        {
+            if (!_isPlayerSmall.Value)
+            {
+                _myAnimator.SetTrigger("down");
+                _myCollider.size = new Vector2(3.6f, 3.0f);
+                _myCollider.offset = new Vector2(1.6f, 1.5f);
+            }
+        }
+    }
+}
