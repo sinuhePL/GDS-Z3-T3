1f3f253656242965325cc611e159f284b94935eb
sinuhePL <lawicki.karol@gmail.com>
2021-09-12 20:20:42 +0200

    Added tutorial tree prefab

---
+++ Assets/Scripts/TutorialBirchController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace GDS3
+{
+    public class TutorialBirchController : MonoBehaviour
+    {
+        [SerializeField] private TextMeshPro _myTextBox;
+        [SerializeField] private string _myText; 
+
+        private void Start()
+        {
+            _myTextBox.text = "";
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Player")
+            {
+                _myTextBox.text = _myText;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                _myTextBox.text = "";
+            }
+        }
+    }
+}
