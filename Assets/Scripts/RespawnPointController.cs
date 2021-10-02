cc0f86d0ece0d520b19a5970732a42dc57803355
sinuhePL <lawicki.karol@gmail.com>
2021-10-02 23:45:07 +0200

    Added respawn point vfx

--- Assets/Scripts/RespawnPointController.cs
+++ Assets/Scripts/RespawnPointController.cs
@@ -9,12 +9,14 @@ namespace GDS3
     {
         [SerializeField] private Vector3Reference _lastRespawnPoint;
         [SerializeField] private LayerMask _targetLayer;
+        [SerializeField] private ParticleSystem _respawnParticleSystem;
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
             if(_targetLayer == (_targetLayer | (1 << collision.gameObject.layer)))
             {
                 _lastRespawnPoint.Value = collision.gameObject.transform.position;
+                _respawnParticleSystem.Play();
             }
         }
     }
