69c36bf501edd2c205a152ebf87da240da68e580
sinuhePL <lawicki.karol@gmail.com>
2021-10-07 13:54:39 +0200

    Added enemy spawners

---
+++ Assets/Scripts/EnemySpawnerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GDS3
+{
+    public class EnemySpawnerController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _enemy;
+        private bool _isEnemyKilled = false;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Player" && !_isEnemyKilled)
+            {
+                _enemy.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                _enemy.SetActive(false);
+            }
+        }
+
+        public void SetKilledFlag()
+        {
+            _isEnemyKilled = true;
+        }
+    }
+}
