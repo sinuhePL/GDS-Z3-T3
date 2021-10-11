a09291370a2ff949e8e3dde08d51e15e915845b6
sinuhePL <lawicki.karol@gmail.com>
2021-10-11 20:03:09 +0200

    Improvements to moving branch

--- Assets/Scripts/BranchController.cs
+++ Assets/Scripts/BranchController.cs
@@ -8,24 +8,31 @@ namespace GDS3
     {
         public BoolReference _isPlayerSmall;
         public Animator _myAnimator;
-        public BoxCollider2D _myCollider;
+        public EdgeCollider2D _myCollider;
+        public EdgeCollider2D _myBottomCollider;
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnCollisionEnter2D(Collision2D collision)
         {
-            if(collision.gameObject.tag == "Player")
+            if (collision.gameObject.tag == "Player" && !_isPlayerSmall.Value)
             {
-                BendBranch();
+                if (collision.otherCollider == (_myCollider as Collider2D))
+                {
+                    _myAnimator.SetTrigger("down");
+                }
             }
         }
 
-        public void BendBranch()
+        public void DeactivateCollider()
         {
-            if (!_isPlayerSmall.Value)
-            {
-                _myAnimator.SetTrigger("down");
-                _myCollider.size = new Vector2(3.6f, 3.0f);
-                _myCollider.offset = new Vector2(1.6f, 1.5f);
-            }
+            _myCollider.enabled = false;
+            _myBottomCollider.enabled = false;
+            _myAnimator.ResetTrigger("down");
+        }
+
+        public void ActivateCollider()
+        {
+            _myCollider.enabled = true;
+            _myBottomCollider.enabled = true;
         }
     }
 }
