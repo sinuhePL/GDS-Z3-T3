65f9d3925076354c8c45f757b141e6e72b15faa1
sinuhePL <lawicki.karol@gmail.com>
2021-10-12 16:49:58 +0200

    Added branch bending when size change

--- Assets/Scripts/BranchController.cs
+++ Assets/Scripts/BranchController.cs
@@ -18,7 +18,7 @@ namespace GDS3
         [Range(0.0f, 1.0f)]
         [SerializeField] private float _bendingBranchBackSoundVolume;
 
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnCollisionStay2D(Collision2D collision)
         {
             if (collision.gameObject.tag == "Player" && !_isPlayerSmall.Value)
             {
